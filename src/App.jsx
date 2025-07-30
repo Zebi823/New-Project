07e9c476a98962f70cdc78dacbb4de321691ea91
zebii <zebaa3438@gmail.com>
2025-07-30 07:35:55 +0530

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './Style.css';
+import { BrowserRouter ,Route,Routes} from 'react-router-dom';
+import Login from "./Pages/Login"
 function App() {
 
 
   return (
     <>
-      
+      <BrowserRouter>
+         <Routes>
+          <Route  path='/login'  element={<Login/>} />
+         </Routes>
+      </BrowserRouter>
     </>
   )
 }
