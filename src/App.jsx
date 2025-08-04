db5aa5eb6a969460acbadfab06908e67f90f71fa
zebii <zebaa3438@gmail.com>
2025-08-04 16:46:54 +0530

    A

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import './Style.css';
 import { BrowserRouter ,Route,Routes} from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
 import Login from "./Pages/Login"
 import HomePage from './Pages/HomePage';
 import AdminDashboard  from './Pages/admin/Dashboard';
@@ -17,17 +16,19 @@ function App() {
     <>
       <BrowserRouter>
          <Routes>
-          <Route  path='/login'  element={<Login/>} />
+         
+           <Route path="/product/:id" element={<ProductDetails />} />
              <Route  path='/'  element={<HomePage/>} />
+              <Route  path='/login'  element={<Login/>} />
 
              {/* private routes */}
-             <Route path="/private" element={<PrivateRoute />}/>
              <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/admin/products" element={<ProductsList />} />
           <Route path="/admin/productcards" element={<ProductCards />} />
            <Route path="/admin/products/add" element={<AddProduct/>} />
           <Route path="/admin/products/edit/:id" element={<EditProduct />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-               <Route path="/admin/products" element={<ProductsList />} />
+           
+            
          </Routes>
       </BrowserRouter>
     </>
